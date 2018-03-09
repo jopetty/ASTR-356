4bd09ce4b50346a8f564abcf79391f62db6565d5
Jackson Petty <jacksonopetty@gmail.com>
2018-03-08 20:41:44 -0500

    I'm very ready for break

---
+++ Assignment 3/main.py
@@ -0,0 +1,11 @@
+## main.py
+
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy as sp
+import scipy.stats
+from scipy.stats import probplot, anderson, shapiro
+import csv
+import math
+from astroML.plotting import hist
+import statsmodels.api as sm
